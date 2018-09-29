048852032a0cf0f57cb278982622b55f8bd93dff
Brian Zinn <github@wakeskate.com>
2018-09-29 09:39:43 -0700

    Add: Include HMR image on main page.

--- src/home/components/index.js
+++ src/home/components/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Home = () => (
-  <div>
-    <h3>Thanks for visiting - Check the examples from the menus</h3>
-    
-    <p>Check out the examples for Virtual Reality, 3D models, 3D UI controls, Skybox and mesh creations using BabylonJS on WebGL running in a React App.</p>
+  <div>   
+    <p>There are examples for Virtual Reality, 3D models, 3D UI controls, Skybox and mesh creations using BabylonJS on WebGL running in a React App.</p>
 
     <p>Interesting part is the UI 2D, where a WebGL texture is dynamically updating from props - best experienced in VR mode :)</p>
+
+    <p>
+    Also, full HMR experience with browser udpating while you build your application.
+    ![BabylonJS HMR](https://raw.githubusercontent.com/brianzinn/react-babylonjs/master/media/react-babylonjs-hmr.gif)
+    </p>
     
     <p className="text-muted small">More coming soon...</p>
   </div>
