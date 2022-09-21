57766b7c478fe59639b2c036c0c69eb25143ec70
Li Zheng <flyskywhy@gmail.com>
2022-09-21 20:26:38 +0800

    react -> react-native: add @flyskywhy/react-native-gcanvas@2.3.25

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependencies: {
+    '@flyskywhy/react-native-gcanvas': {
+      platforms: {
+        android: {
+          packageImportPath: 'import com.taobao.gcanvas.bridges.rn.GReactPackage;',
+        },
+      },
+    },
+  },
+};
