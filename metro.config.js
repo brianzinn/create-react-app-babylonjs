e037acdfe41af036e268bdfe475774e149be0939
Li Zheng <flyskywhy@gmail.com>
2022-12-20 15:07:54 +0800

    react -> react-native: support `npm run web-clean` on Windows and avoid error `node_modules\sane\src\node_watcher.js:291:16`

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@
  *
  * @format
  */
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
   transformer: {
@@ -17,5 +18,16 @@ module.exports = {
   resolver: {
     assetExts: ['dds', 'txt', 'jpg', 'png'],
     sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
+    blacklistRE: blacklist([
+      // to avoid error below when `npm run web-clean` besides `npm run rn` on Windows
+      //events.js:174
+      //       throw er; // Unhandled 'error' event
+      //       ^
+      // Error: EPERM: operation not permitted, lstat 'D:\proj\GCanvasRNExamples\node_modules\.cache\default-development'
+      // Emitted 'error' event at:
+      //     at NodeWatcher.<anonymous> (D:\proj\GCanvasRNExamples\node_modules\sane\src\node_watcher.js:291:16)
+      //     at FSReqWrap.oncomplete (fs.js:153:21)
+      /node_modules\/\.cache/,
+    ]),
   },
 };
